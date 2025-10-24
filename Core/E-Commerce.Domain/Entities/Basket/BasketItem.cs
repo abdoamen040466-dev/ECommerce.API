80ff0ec64c3348ce841ad4191baff978851eb42c
abdallah-amin <abdoamen040466@gmail.com>
2025-10-24 18:16:57 +0300

    Basket Entity

---
+++ Core/E-Commerce.Domain/Entities/Basket/BasketItem.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce.Domain.Entities.Basket;
+public class BasketItem
+{
+#nullable disable
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string PictureUrl { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Core/E-Commerce.Domain/Entities/Basket/CustomerBasket.cs
@@ -0,0 +1,6 @@
+﻿namespace E_Commerce.Domain.Entities.Basket;
+public class CustomerBasket
+{
+    public string Id { get; set; } = default!;
+    public ICollection<BasketItem> Items { get; set; } = [];
+}
