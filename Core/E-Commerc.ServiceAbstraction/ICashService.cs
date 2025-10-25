6106d025177e7cab088b60c5b6f9b61b92eea97c
abdallah-amin <abdoamen040466@gmail.com>
2025-10-25 17:29:07 +0300

    Cash Service

---
+++ Core/E-Commerc.ServiceAbstraction/ICashService.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerc.ServiceAbstraction;
+public interface ICashService
+{
+    Task<string?> GetAsync(string key);
+    Task SetAsync(string key, object value, TimeSpan TTL);
+
+}
---
+++ Infrastructure/E-Commerce.Presistense/Service/CashService.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerc.ServiceAbstraction;
+using StackExchange.Redis;
+
+namespace E_Commerce.Presistense.Service;
+internal class CashService(IConnectionMultiplexer multiplexer) : ICashService
+{
+
+    private readonly IDatabase _database = multiplexer.GetDatabase();
+    public async Task<string?> GetAsync(string key)
+    {
+        return await _database.StringGetAsync(key);
+    }
+
+    public async Task SetAsync(string key, object value, TimeSpan TTL)
+    {
+        var option = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        var json = JsonSerializer.Serialize(value, option);
+        await _database.StringSetAsync(key, json, TTL);
+    }
+}
