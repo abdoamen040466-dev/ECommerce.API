9058bba781b7eb6b3d01478a2783ad5a487778b6
abdallah-amin <abdoamen040466@gmail.com>
2025-10-25 16:01:13 +0300

    Basket Controller

---
+++ Infrastructure/E-Commerce.Presentation.API/Controllers/BasketsController.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerc.ServiceAbstraction;
+using E_Commerce.Shared.Basket;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Presentation.API.Controllers;
+public class BasketsController(IBasketService basketService) : APIBaseController
+{
+    [HttpPost]
+    public async Task<ActionResult<CustomerBasketDTO>> update(CustomerBasketDTO basketDTO)
+    {
+        return Ok(await basketService.CreateOrUpdateAsync(basketDTO));
+    }
+    [HttpGet]
+    public async Task<ActionResult<CustomerBasketDTO>> Get(string id)
+    {
+        return Ok(await basketService.GetByIdAsync(id));
+    }
+    // Post baseUrl/api/Baskets?id=value
+    [HttpDelete]
+    public async Task<ActionResult> Delete(string id)
+    {
+        await basketService.DeleteAsync(id);
+        return NoContent();
+    }
+
+}
