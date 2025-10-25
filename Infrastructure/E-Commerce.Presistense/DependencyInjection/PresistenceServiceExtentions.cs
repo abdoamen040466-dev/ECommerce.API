a4f47d094e13ffaaa44d05c25b8ea1cb92aa784c
abdallah-amin <abdoamen040466@gmail.com>
2025-10-25 15:13:52 +0300

    Basket Repository

--- Infrastructure/E-Commerce.Presistense/DependencyInjection/PresistenceServiceExtentions.cs
+++ Infrastructure/E-Commerce.Presistense/DependencyInjection/PresistenceServiceExtentions.cs
@@ -1,9 +1,17 @@
-﻿namespace E_Commerce.Presistense.DependencyInjection;
+﻿using StackExchange.Redis;
+
+namespace E_Commerce.Presistense.DependencyInjection;
 public static class PresistenceServiceExtentions
 {
     public static IServiceCollection AddPresistenceServices(this IServiceCollection service
         , IConfiguration configuration)
     {
+
+        service.AddSingleton<IConnectionMultiplexer>(cfg =>
+        {
+            return ConnectionMultiplexer.Connect(configuration
+                   .GetConnectionString("RedisConnection")!);
+        });
         service.AddDbContext<ApplicationDbContext>(options =>
         {
             var connection = configuration.GetConnectionString("SQLConnection");
