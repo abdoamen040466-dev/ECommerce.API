56e5db1df2e09fb1b7dc76f05deaec35107af3bb
abdallah-amin <abdoamen040466@gmail.com>
2025-11-14 18:54:06 +0200

    carete User Controller

---
+++ Infrastructure/E-Commerce.Presentation.API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using E_Commerc.ServiceAbstraction;
+using E_Commerce.Shared.DataTransferObject.Auth;
+using E_Commerce.Shared.DataTransferObject.Users;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace E_Commerce.Presentation.API.Controllers;
+internal class UsersController(IUserService userService)
+    : APIBaseController
+{
+    [HttpGet]
+    public async Task<ActionResult<UserResponse>> GetUser()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+
+        var result = await userService.GetByEmailAsync(email!);
+        return HandleResult(result);
+    }
+    [HttpGet("Address")]
+    public async Task<ActionResult<AddressDTO>> GetAddress()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+
+        var result = await userService.GetAddessAsync(email!);
+        return HandleResult(result);
+    }
+    [HttpPut("Address")]
+    public async Task<ActionResult<AddressDTO>> UpdateAddress(AddressDTO address)
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+
+        var result = await userService.UpdateAddressAsync(email!, address);
+        return HandleResult(result);
+    }
+
+}
